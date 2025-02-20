6bf7b20661d0b96e7ea7711394458b0b4fe63dea
Ane Marinheiro Weber <anemariweber@gmail.com>
2025-02-20 22:23:26 +0100

    Put, Delete and Summary routes

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-#RF
+# Diet API
+Diet API is a backend application designed to keep track of a diet routine.
 
+## Technologies Used
+- Node.js
+- Typescript
+- Fastify
+- Knex
+- SQLite
+- Zod
+- Isomnia file available.
 
-#RN
+## Requirements and Functionalities
+[x] Create a user
+[x] Identify user between transactions using cookies
+[x] Create a meal for a specific user
+[x] A meal must have a title, description, date, and indicate if it is on the diet
+[x] Edit meals
+[x] Delete a meal
+[x] Provide a summary with metrics for each user
+[x] Users can only see and edit meals they have created
--- src/routes/meals-routes.ts
+++ src/routes/meals-routes.ts
@@ -37,6 +37,7 @@ export async function mealsRoutes(app: FastifyInstance) {
   })
 
   //selecting all meals from one user
+  //add count, add count is on diet
   app.get('/', 
     { 
     preHandler: [checkSessionIdExists], 
@@ -70,7 +71,112 @@ export async function mealsRoutes(app: FastifyInstance) {
     return reply.send({ meal })
   })
 
-  //put
-  //delete
-  //get metrics - amount of meals per day - amount of meals on diet
+  //Getting all the metrics
+  app.get('/summary',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request, reply) => {
+      const totalMealsOnDiet = await knex('meals')
+        .where({ user_id: request.user?.id, is_on_diet: true })
+        .count('id', { as: 'total' })
+        .first()
+
+      const totalMealsOffDiet = await knex('meals')
+        .where({ user_id: request.user?.id, is_on_diet: false })
+        .count('id', { as: 'total' })
+        .first()
+
+      const totalMeals = await knex('meals')
+        .where({ user_id: request.user?.id })
+        .orderBy('date', 'desc')
+
+      const { bestOnDietSequence } = totalMeals.reduce(
+        (acc, meal) => {
+          if (meal.is_on_diet) {
+            acc.currentSequence += 1
+          } else {
+            acc.currentSequence = 0
+          }
+
+          if (acc.currentSequence > acc.bestOnDietSequence) {
+            acc.bestOnDietSequence = acc.currentSequence
+          }
+
+          return acc
+        },
+        { bestOnDietSequence: 0, currentSequence: 0 },
+      )
+
+      return reply.send({
+        totalMeals: totalMeals.length,
+        totalMealsOnDiet: totalMealsOnDiet?.total,
+        totalMealsOffDiet: totalMealsOffDiet?.total,
+        bestOnDietSequence,
+      })
+    },
+  )
+
+  //updating info from one specific meal
+  app.put('/:mealId', 
+    {
+     preHandler: [checkSessionIdExists], 
+    }, 
+    async (request, reply) => {
+      const paramsSchema = z.object({
+        mealId: z.string().uuid(),
+      })
+  
+      const { mealId } = paramsSchema.parse(request.params)
+  
+      const updateMealBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        isOnDiet: z.boolean(),
+        date: z.coerce.date(),
+      })
+      
+      const {name, description, isOnDiet, date} = updateMealBodySchema.parse(
+        request.body
+      )
+      
+      const meal = await knex('meals').where({id: mealId}).first()
+      
+      if (!meal) {
+        return reply.status(404).send({ error: 'Meal not found'})
+      }
+
+      await knex('meals').where({id: mealId}).update({
+        name,
+        description,
+        is_on_diet: isOnDiet,
+        date: date.getTime()
+      })
+
+      return reply.status(204).send()
+  })
+
+  //deleting a meal
+  app.delete('/:mealId', 
+    { 
+      preHandler: [checkSessionIdExists], 
+    }, 
+    async (request, reply) => {
+    const paramsSchema = z.object({
+      mealId: z.string().uuid(),
+    })
+
+    const { mealId } = paramsSchema.parse(request.params)
+
+    const meal = await knex('meals').where({id: mealId}).first()
+
+    if (!meal) {
+      return reply.status(404).send({ error: 'Meal not found'})
+    }
+
+    await knex('meals').where({id: mealId}).delete()
+
+    return reply.status(204).send()
+  })
+
 }
